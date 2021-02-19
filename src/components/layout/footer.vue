649f3c6aec24c8003906bff68cc3ba63611e5888
橙色阳光 <1122@hizhou.cn>
2021-02-19 22:45:32 +0800

    📦 footer的copyright

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -1,16 +1,55 @@
 <template>
-  <footer class="layout-footer" />
+  <footer class="layout-footer">
+    <div
+      class="copyright"
+      v-html="copyrightString"
+    />
+  </footer>
 </template>
 
 <script>
 /**
  * Layout Footer
  */
+
+import {
+  mapState,
+} from 'vuex';
+
 export default {
   name: 'LayoutFooter',
+  computed: {
+    ...mapState({
+      domainString(state) {
+        if (state.zbp.host) {
+          return state.zbp.host.replace(/http(s)*:\/\//, '').slice(0, -1);
+        }
+        return '';
+      },
+    }),
+    yearString() {
+      return new Date().getFullYear();
+    },
+    copyrightString() {
+      if (window.zbpCopyright && window.zbpCopyright !== '{$copyright}') {
+        return window.zbpCopyright;
+      }
+      return `&copy; ${this.yearString} ${this.domainString}`;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.layout-footer {
+  padding: 30px 0;
+  margin-top: 30px;
+  background: #fff;
 
+  .copyright {
+    text-align: center;
+    font-size: 14px;
+    color: #aaa;
+  }
+}
 </style>
